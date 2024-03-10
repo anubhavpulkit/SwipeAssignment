9aa9ec9015a51a800e291ec612625ae71a1b8997
anubhavpulkit <anubhavssingh177@gmail.com>
2024-03-10 18:12:35 +0530

    Initial Commit

---
+++ SwipeCat/SwipeCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeCatApp.swift
+//  SwipeCat
+//
+//  Created by Anubhav Singh on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwipeCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
